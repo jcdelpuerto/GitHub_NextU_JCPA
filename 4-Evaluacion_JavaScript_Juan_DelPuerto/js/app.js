e3ee74105fed36da2ef0335ec5a9a1a50d46bdcc
jcdelpuerto <demodata@hotmail.com>
2017-08-12 02:43:32 -0500

    evaluacion completa
    evaluación javascript concluida

--- 4-Evaluacion_JavaScript_Juan_DelPuerto/js/app.js
+++ 4-Evaluacion_JavaScript_Juan_DelPuerto/js/app.js
@@ -4,8 +4,11 @@ var numero1 = 0;
 var numero2 = 0;
 var resultado = 0;
 var operacion = "no";
+var operacionDosNumeros = false
 var largoNum1 = 1;
 var largoNum2 = 0;
+var largoResultado = 0;
+var operacionRealizada = false;
 
 //poner las teclas en un arreglo
 var teclas = document.getElementsByClassName('tecla');
@@ -60,25 +63,23 @@ Calculadora = (function(){
           switch (agregarNumeroPantalla) {
             case "On":
               pantalla.innerHTML = "0";
-              tienePunto = false;
-              numero1 = 0;
-              numero2 = 0;
-              resultado = 0;
-              operacion = "no";
-              largoNum1 = 1;
-              largoNum2 = 0;
-              esNegativo = false;
+              limpiaVariables()
               break;
 
             case "punto":
               if (tienePunto == false) {
-                pantalla.innerHTML = valorEnPantalla + ".";
+                if (valorEnPantalla != ""){
+                    pantalla.innerHTML = valorEnPantalla + ".";
+                }
+                else {
+                  pantalla.innerHTML = valorEnPantalla + "0.";
+                }
                 tienePunto = true;
               }
               break;
 
             case "signo":
-              if (valorEnPantalla != "0" && valorEnPantalla != "0." && valorEnPantalla != "" ){
+              if (valorEnPantalla != "0" && valorEnPantalla != "0." && valorEnPantalla != "" && operacionRealizada == false){
                 if (operacion == 'no'){
                   numero1 = numero1 * (-1);
                   pantalla.innerHTML = numero1.toString();
@@ -94,46 +95,85 @@ Calculadora = (function(){
                   esNegativo = false;
                 }
               }
+              else if (operacionRealizada == true){
+                numero1 = numero1 * (-1);
+                pantalla.innerHTML = numero1.toString();
+                if (esNegativo == false){
+                  esNegativo = true;
+                }
+                else{
+                  esNegativo = false;
+                }
+              }
               break;
 
             case "mas":
-
+              operacion = "+";
+              limpiaVariables2()
+              pantalla.innerHTML = "";
               break;
 
             case "menos":
+              operacion = "-";
+              limpiaVariables2()
+              pantalla.innerHTML = "";
 
               break;
 
             case "por":
+              operacion = "*";
+              limpiaVariables2()
+              pantalla.innerHTML = "";
 
               break;
 
             case "dividido":
+              operacion = "/";
+              limpiaVariables2()
+              pantalla.innerHTML = "";
 
               break;
 
             case "raiz":
-
+              var operacionPrevia = operacion
+              operacion = "sqr";
+              operacionDosNumeros = false;
+              if (esNegativo == true){
+                alert("No se puede calcula raiz cuadrada de número negativo")
+                operacion = operacionPrevia;
+                operacionDosNumeros = true;
+              }
+              else if (valorEnPantalla == ""){
+                alert("No hay número para calcular")
+                operacion = operacionPrevia;
+                operacionDosNumeros = true;
+              }
+              else {
+                resultado = raizCuadrada(valorEnPantalla);
+                operacionRealizada = true;
+                pantalla.innerHTML = resultado;
+              }
               break;
 
             case "igual":
-
+              if (operacionDosNumeros = true){
+                resultado = operaNumeros(numero1,numero2);
+                operacionRealizada = true;
+              }
+              pantalla.innerHTML = resultado;
               break;
 
             case "0":
               if (valorEnPantalla != '0') {
-
                 if (operacion == 'no'  && largoNum1 < 8 ){
                   pantalla.innerHTML = valorEnPantalla + agregarNumeroPantalla ;
                   numero1 = parseFloat(pantalla.innerHTML);
-                  var largo5 = numero1.toString();
-                  largoNum1 = largo5.length;
+                  largoNum1 = largoNumero(numero1);
                 }
                 else if (operacion != 'no' && largoNum2 < 8 ){
                   pantalla.innerHTML = valorEnPantalla + agregarNumeroPantalla ;
                   numero2 = parseFloat(pantalla.innerHTML);
-                  var largo6 = numero2.toString();
-                  largoNum2 = largo6.length;
+                  largoNum2 = largoNumero(numero2);
                 }
               }
               break;
@@ -143,32 +183,106 @@ Calculadora = (function(){
                 pantalla.innerHTML = agregarNumeroPantalla ;
                 if (operacion == 'no'){
                   numero1 = parseFloat(pantalla.innerHTML);
-                  var largo1 = numero1.toString();
-                  largoNum1 = largo1.length;
+                  largoNum1 = largoNumero(numero1);
                 }
                 else {
                   numero2 = parseFloat(pantalla.innerHTML);
-                  var largo2 = numero2.toString();
-                  largoNum2 = largo2.length;
+                  largoNum2 = largoNumero(numero2);
                 }
               }
               else if (valorEnPantalla != '0') {
                 if (operacion == 'no' && largoNum1 < 8 ){
                   pantalla.innerHTML = valorEnPantalla + agregarNumeroPantalla ;
                   numero1 = parseFloat(pantalla.innerHTML);
-                  var largo3 = numero1.toString();
-                  largoNum1 = largo3.length;
+                  largoNum1 = largoNumero(numero1);
                 }
-                else if (operacion != 'no' && largoNum2 < 8 ){
+                else if (operacionRealizada == false && operacion != 'no' && largoNum2 < 8 ){
                   pantalla.innerHTML = valorEnPantalla + agregarNumeroPantalla ;
                   numero2 = parseFloat(pantalla.innerHTML);
-                  var largo4 = numero2.toString();
-                  largoNum2 = largo4.length;
+                  largoNum2 = largoNumero(numero2);
+                }
+                else if (operacionRealizada == true){
+                  limpiaVariables()
+                  pantalla.innerHTML = agregarNumeroPantalla ;
+                  numero1 = parseFloat(pantalla.innerHTML);
+                  largoNum1 = largoNumero(numero1);
                 }
               }
               break;
           }
 
       }
+      function operaNumeros(x,y){
+        var z = 0
+        switch (operacion) {
+          case "+":
+            z = x + y
+          break;
+          case "-":
+            z = x - y
+          break;
+          case "*":
+            z = x * y
+          break;
+          case "/":
+            z = x / y
+          break;
 
+          default:
+        }
+        largoResultado = largoNumero(z);
+        while (largoResultado > 8){
+          var valorCadena = z.toString();
+          valorCadena = valorCadena.substring(0,8);
+          z = parseFloat(valorCadena);
+          largoResultado = largoNumero(z);
+        }
+        numero1 = z;
+        largoNum1 = largoNumero(numero1);
+
+        return z;
+      }
+      function raizCuadrada(x){
+        var z = 0
+        z = Math.sqrt(x);
+        largoResultado = largoNumero(z);
+        while (largoResultado > 8){
+          var valorCadena = z.toString();
+          valorCadena = valorCadena.substring(0,8);
+          z = parseFloat(valorCadena);
+          largoResultado = largoNumero(z);
+        }
+        numero1 = z;
+        largoNum1 = largoNumero(numero1);
+
+        return z;
+
+      }
+      function largoNumero(x){
+        var valorCadena = x.toString();
+
+        return  valorCadena.length;
+      }
+      function limpiaVariables(){
+        tienePunto = false;
+        esNegativo = false;
+        numero1 = 0;
+        numero2 = 0;
+        resultado = 0;
+        operacion = "no";
+        operacionDosNumeros = false;
+        largoNum1 = 1;
+        largoNum2 = 0;
+        largoResultado = 0;
+        operacionRealizada = false;
+      }
+      function limpiaVariables2(){
+        operacionDosNumeros = true;
+        numero2 = 0;
+        resultado = 0;
+        largoNum2 = 0;
+        largoResultado = 0;
+        operacionRealizada = false;
+        esNegativo = false;
+      }
 }());
