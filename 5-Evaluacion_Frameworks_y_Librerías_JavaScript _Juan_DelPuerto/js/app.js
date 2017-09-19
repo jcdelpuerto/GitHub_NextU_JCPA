bda2fd52d04a66bbd1233b1a717c42cde8892796
jcdelpuerto <demodata@hotmail.com>
2017-09-19 00:50:21 -0500

    Inicia parte 2
    Llenado aleatorio de imagenes

--- 5-Evaluacion_Frameworks_y_Librerías_JavaScript _Juan_DelPuerto/js/app.js
+++ 5-Evaluacion_Frameworks_y_Librerías_JavaScript _Juan_DelPuerto/js/app.js
@@ -1,25 +1,47 @@
+$(document).ready(function(){
+  juegoDulces.init();
+});
 
-
+var numColumnas = 7;
+var numFilas = 5;
 
 var juegoDulces = {
   init: function(){
     this.colorTitulo();
+    this.mostrarImagenes();
   },
 
-  //El título “Match Game” debe tener una animación que cambie de color después de determinado tiempo,
-  //posteriormente vuelva al color original, y permanezca cambiando entre dos colores indefinidamente.
   colorTitulo: function(){
     setInterval(function(){
       $(".main-titulo").switchClass("main-titulo","main-titulo-blanco", 350),
       $(".main-titulo-blanco").switchClass("main-titulo-blanco","main-titulo", 350)
       }, 1000);
+    },
+
+    mostrarImagenes: function(){
+      var contador = 1;
+      var numImg = 1;
+      for(var i = 1; i <= numColumnas; i++){
+        for(var j = 1; j <= numFilas; j++){
+          var imagen = new this.imagenesDulces;
+          var src = imagen[Math.floor(Math.random() * imagen.total)];
+          $(".col-" + contador).append("<div id='item-"+ numImg +" img-"+ j +"'><img src="+ src +" class='imagen-"+ numImg +"'></div>");
+
+          numImg++;
+        }
+        contador++;
+      }
+    },
+
+    imagenesDulces: function(){
+      var i = 0;
+      this[i++] = "image/1.png";
+      this[i++] = "image/2.png";
+      this[i++] = "image/3.png";
+      this[i++] = "image/4.png";
+      this.total = i;
     }
+
+
+
   }
-
-
-
-
-
-  $(document).ready(function(){
-    juegoDulces.init();
-  });
